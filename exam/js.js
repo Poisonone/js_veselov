06022387efc032bdb15d22acb972f3446ec4aaef
Sasha Veselov <realcool2222@gmail.com>
2021-02-17 20:29:42 +0200

    Initial commit

---
+++ exam/js.js
@@ -0,0 +1,25 @@
+    function sum(){
+    var first = document.getElementById("first").value;
+    var second = document.getElementById("second").value;
+    var res = parseInt(first)+parseInt(second);
+    document.getElementById("result").innerHTML = res;
+
+}
+    function min(){
+    var first = document.getElementById("first").value;
+    var second = document.getElementById("second").value;
+    var res = parseInt(first)+parseInt(second);
+    alert(parseInt(res));
+}
+    function div(){
+    var first = document.getElementById("first").value;
+    var second = document.getElementById("second").value;
+    var res = parseInt(first)+parseInt(second);
+    alert(parseInt(res));
+}
+    function mult(){
+    var first = document.getElementById("first").value;
+    var second = document.getElementById("second").value;
+    var res = parseInt(first)+parseInt(second);
+    alert(parseInt(res));
+}
